901c3e951f52392e29d39d5a6e1c6df60b8e2104
unknown <wyzypa@gmail.com>
2019-11-16 19:23:12 +0900

    20191116 add remove-bpe argument to generating phase of ibendao.py

--- ibendao.py
+++ ibendao.py
@@ -10,6 +10,11 @@ import os, sys, warnings, datetime, time
 ########################################################
 # In the following block, change basic settings of the experiment
 #######################################################
+## extra environment variables
+EXTRA_EV = {}
+for k,v in EXTRA_EV.items():
+    os.environ.setdefault(k, v)
+
 ## basic settings
 CUDA_VISIBLE_DEVICES = '0,1'
 TASK_NAME = 'iwslt14-de2en'
@@ -47,9 +52,10 @@ SOLID_TRAIN_SETTINGS = {
 }
 
 GENERATE_SETTINGS = {
-    '--batch-size': 128,
+    '--batch-size': 32,
     '--beam': 5,
-    '--lenpen': 1.0
+    '--lenpen': 1.0,
+    '--remove-bpe': True
 }
 ################# end of block #################
 
@@ -82,74 +88,87 @@ def concat_cmd(root, **kwargs):
     return cmd
 
 def main():
-    input('You will train a [{}] with task [{}]. Press Enter to continue...'.format(MODEL_VAR, TASK_NAME))
 
-    os.system('export CUDA_VISIBLE_DEVICES="{}"'.format(CUDA_VISIBLE_DEVICES))
-
-    model_output_dir = os.path.join(BASE_DIR, TASK_NAME, MODEL_VAR, 'model')
-    log_output_dir = os.path.join(BASE_DIR, TASK_NAME, MODEL_VAR, 'logs')
-    for d in (model_output_dir, log_output_dir):
-        if not os.path.isdir(d):
-            os.system('mkdir -p \'{}\''.format(d))
-
-    data_bin_dir = os.path.join(BASE_DIR, 'data-bin') if 'data-bin' in os.listdir(BASE_DIR) else \
-        os.path.join(BASE_DIR, TASK_NAME, MODEL_VAR, 'data-bin')
-    warnings.warn('Using data-bin @ [{}]'.format(data_bin_dir))
-
-    # prepare for training
-    train_cmd = 'python -u {}/train.py {}'.format(FAIRSEQ_NAME, data_bin_dir)
-    TRAIN_SETTINGS.update(SOLID_TRAIN_SETTINGS)
-    TRAIN_SETTINGS['--save-dir'] = model_output_dir
-    train_cmd = concat_cmd(train_cmd, **TRAIN_SETTINGS)
-    train_log_fn = os.path.join(log_output_dir, 'train.log.{}'.format(datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
-    train_cmd = '{} 2>&1 | tee {}'.format(train_cmd, train_log_fn)
-    print(train_cmd)
-    for i in range(5):
-        time.sleep(0.5)
-        print('.', end='')
-
-    # start training
-    train_start_at = time.time()
-    rtn_code = os.system(train_cmd)
-    train_end_at = time.time()
-    train_period = train_end_at - train_start_at
-    if rtn_code == 0:
-        print('Training is done in [{:.4f}] seconds'.format(train_period))
+    steps = []
+    try:
+        step = sys.argv[1]
+    except IndexError as e:
+        steps = ['train', 'generate']
     else:
-        print('Training stopped for error.')
-        sys.exit(1)
+        steps.append(step)
 
-    if DECODE_LAST_ENSEMBLE == -1:    # use checkpoint_best.pt as trained model
-        model = os.path.join(model_output_dir, 'checkpoint_best.pt')
-    else:
-        print('Generating last {} ensembles'.format(DECODE_LAST_ENSEMBLE))
-        model = os.path.join(model_output_dir, 'checkpoint.ensemble{}.pt'.format(DECODE_LAST_ENSEMBLE))
-        ensemble_cmd = 'python -u {}/scripts/average_checkpoints.py --inputs {} --output {} --num-epoch-checkpoints {}'\
-            .format(FAIRSEQ_NAME, model_output_dir, model, DECODE_LAST_ENSEMBLE)
-        rtn_code = os.system(ensemble_cmd)
-        if rtn_code != 0:
-            print('Failed to ensemble.')
+    if 'train' in steps:
+        input('You will train a [{}] for task [{}] with GPU [{}]. The fairseq components are [{}]. '
+              'Press Enter to continue...'.format(MODEL_VAR, TASK_NAME, CUDA_VISIBLE_DEVICES, FAIRSEQ_NAME))
+
+        # os.system('export CUDA_VISIBLE_DEVICES="{}"'.format(CUDA_VISIBLE_DEVICES))
+        os.environ.setdefault('CUDA_VISIBLE_DEVICES', CUDA_VISIBLE_DEVICES)
+
+        model_output_dir = os.path.join(BASE_DIR, TASK_NAME, MODEL_VAR, 'model')
+        log_output_dir = os.path.join(BASE_DIR, TASK_NAME, MODEL_VAR, 'logs')
+        for d in (model_output_dir, log_output_dir):
+            if not os.path.isdir(d):
+                os.system('mkdir -p \'{}\''.format(d))
+
+        data_bin_dir = os.path.join(BASE_DIR, 'data-bin') if 'data-bin' in os.listdir(BASE_DIR) else \
+            os.path.join(BASE_DIR, TASK_NAME, MODEL_VAR, 'data-bin')
+        warnings.warn('Using data-bin @ [{}]'.format(data_bin_dir))
+
+        # prepare for training
+        train_cmd = 'python -u {}/train.py {}'.format(FAIRSEQ_NAME, data_bin_dir)
+        TRAIN_SETTINGS.update(SOLID_TRAIN_SETTINGS)
+        TRAIN_SETTINGS['--save-dir'] = model_output_dir
+        train_cmd = concat_cmd(train_cmd, **TRAIN_SETTINGS)
+        train_log_fn = os.path.join(log_output_dir, 'train.log.{}'.format(datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
+        train_cmd = '{} 2>&1 | tee {}'.format(train_cmd, train_log_fn)
+        print(train_cmd)
+        for i in range(5):
+            time.sleep(0.5)
+            print('.', end='')
+
+        # start training
+        train_start_at = time.time()
+        rtn_code = os.system(train_cmd)
+        train_end_at = time.time()
+        train_period = train_end_at - train_start_at
+        if rtn_code == 0:
+            print('Training is done in [{:.4f}] seconds'.format(train_period))
+        else:
+            print('Training stopped for error.')
             sys.exit(1)
 
-    # prepare for generating
-    generate_cmd = 'python -u {}/generate.py {}'.format(FAIRSEQ_NAME, data_bin_dir)
-    GENERATE_SETTINGS['--path'] = model
-    generate_cmd = concat_cmd(generate_cmd, **GENERATE_SETTINGS)
-    gen_log_fn = os.path.join(log_output_dir, 'generate.log.{}'.format(datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
-    generate_cmd = '{} 2>&1 | tee {}'.format(generate_cmd, gen_log_fn)
+    if 'generate' in steps:
+        if DECODE_LAST_ENSEMBLE == -1:    # use checkpoint_best.pt as trained model
+            model = os.path.join(model_output_dir, 'checkpoint_best.pt')
+        else:
+            print('Generating last {} ensembles'.format(DECODE_LAST_ENSEMBLE))
+            model = os.path.join(model_output_dir, 'checkpoint.ensemble{}.pt'.format(DECODE_LAST_ENSEMBLE))
+            ensemble_cmd = 'python -u {}/scripts/average_checkpoints.py --inputs {} --output {} --num-epoch-checkpoints {}'\
+                .format(FAIRSEQ_NAME, model_output_dir, model, DECODE_LAST_ENSEMBLE)
+            rtn_code = os.system(ensemble_cmd)
+            if rtn_code != 0:
+                print('Failed to ensemble.')
+                sys.exit(1)
 
-    # start generating
-    gen_start_at = time.time()
-    rtn_code = os.system(generate_cmd)
-    gen_end_at = time.time()
-    gen_period = gen_end_at - gen_start_at
-    if rtn_code == 0:
-        print('Generating is done in [{:.4f}] seconds'.format(gen_period))
-    else:
-        print('Generation stopped for error.')
-        sys.exit(1)
-    
-    os.system('tail -n 1 {} >> {}'.format(gen_log_fn, train_log_fn))
+        # prepare for generating
+        generate_cmd = 'python -u {}/generate.py {}'.format(FAIRSEQ_NAME, data_bin_dir)
+        GENERATE_SETTINGS['--path'] = model
+        generate_cmd = concat_cmd(generate_cmd, **GENERATE_SETTINGS)
+        gen_log_fn = os.path.join(log_output_dir, 'generate.log.{}'.format(datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
+        generate_cmd = '{} 2>&1 | tee {}'.format(generate_cmd, gen_log_fn)
+
+        # start generating
+        gen_start_at = time.time()
+        rtn_code = os.system(generate_cmd)
+        gen_end_at = time.time()
+        gen_period = gen_end_at - gen_start_at
+        if rtn_code == 0:
+            print('Generating is done in [{:.4f}] seconds'.format(gen_period))
+        else:
+            print('Generation stopped for error.')
+            sys.exit(1)
+
+        os.system('tail -n 1 {} >> {}'.format(gen_log_fn, train_log_fn))
 
 
 if __name__ == '__main__':
