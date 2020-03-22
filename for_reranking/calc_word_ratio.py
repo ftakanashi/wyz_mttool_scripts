bfaf33bb937e50fc326194b0eabb33ec19da8912
unknown <wyzypa@gmail.com>
2020-03-22 16:31:43 +0900

    20200322-2

---
+++ for_reranking/calc_word_ratio.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--src', required=True,
+                        help='Path to the source corpus file.')
+    parser.add_argument('--tgt', required=True,
+                        help='Path to the target corpus file.')
+    parser.add_argument('--std-ratio', default=0.0,
+                        help='Will output deviation compared to the standard ratio if is specified.')
+    parser.add_argument('-o', '--output', required=True,
+                        help='Path to the output file.')
+
+    opt = parser.parse_args()
+
+    with open(opt.src, 'r') as f:
+        src_lines = f.readlines()
+
+    with open(opt.tgt, 'r') as f:
+        tgt_lines = f.readlines()
+
+    fw = open(opt.output, 'w')
+
+    std_ratio = float(opt.std_ratio)
+    for i, src in enumerate(src_lines):
+        tgt = tgt_lines[i]
+        src_len = len(src.strip().split())
+        tgt_len = len(tgt.strip().split())
+        ratio = float(src_len) / tgt_len
+        if std_ratio == 0.0:
+            fw.write('{}\n'.format(ratio))
+        else:
+            deviation = abs(( ratio - std_ratio ) / std_ratio)
+            fw.write('{}\n'.format(-deviation))
+
+    fw.close()
+
+if __name__ == '__main__':
+    main()
