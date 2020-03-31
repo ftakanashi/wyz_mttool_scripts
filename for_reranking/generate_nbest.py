de913cd6f85af20afb3489c58acc15e1e3322ec0
unknown <wyzypa@gmail.com>
2020-03-31 15:51:15 +0900

    20200331

--- for_reranking/generate_nbest.py
+++ for_reranking/generate_nbest.py
@@ -4,6 +4,8 @@
 import argparse
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def run(cmd):
     print(cmd)
     os.system(cmd)
@@ -11,6 +13,7 @@ def run(cmd):
 def generate(opt):
     SRC = opt.src
     TGT = opt.tgt
+    SCRIPT_TOOL = opt.tool_script_dir
 
     lengths = opt.len_opt.split(',')
 
@@ -21,11 +24,17 @@ def generate(opt):
         models = os.listdir(os.path.join(opt.model_dir, cate))
         return ':'.join([os.path.join(model_dir, cate, m) for m in models])
 
+    # generate and clean the log
     for beam,best in zip(beam_len, best_len):
-        run('python fairseq/generate.py {} --path {} --max-sentences {} --beam {} --nbest {} | tee generate.{}.log'.format(
+        log_fn = 'generate.{}.log'.format(best)
+        run('python fairseq/generate.py {} --path {} --max-sentences {} --beam {} --nbest {} | tee {}'.format(
             opt.data_bin_dir, ensemble_model_str(opt.model_dir, '{}2{}.l2r'.format(SRC, TGT)),
-            opt.batch_size, beam, best, best
+            opt.batch_size, beam, best, log_fn
         ))
+        hyp_fn = 'hyp.{}.{}'.format(best, TGT)
+        run('python {}/extract_hyp_wei.py -i {} -o {}'.format(log_fn, hyp_fn))
+        run('rm -f {}'.format(log_fn))
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -44,6 +53,7 @@ def main():
                         help='Path to the data-bin.')
     parser.add_argument('--batch-size', default=64, type=int,
                         help='Default batch size.')
+    parser.add_argument('--tool-script-dir', default=os.path.join(BASE_DIR, 'wyz_mttool_scripts'))
 
     opt = parser.parse_args()
 
